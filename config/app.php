47ff8ce618ae6f8ff9158eaba079bb43636ab297
Nicolas Chaima <nikochaima72@gmail.com>
2022-07-09 18:31:20 -0400

    Deploy heroku

--- config/app.php
+++ config/app.php
@@ -3,13 +3,13 @@
 // aqui agregaremos todas las constantes de configuracion de la aplicacion
 
 define("APP_NAME", "ChaFood");
-define("APP_HOST", "http://localhost");
-define("APP_URI", "/chafood");
-define("APP_DOCUMENT_ROOT", "C:\laragon\www\chafood");
+define("APP_HOST", "https://nch-chafood.herokuapp.com/");
+define("APP_URI", "");
+define("APP_DOCUMENT_ROOT", "/");
 
-define("DB_HOST", 'localhost');
-define("DB_DATABASE", 'recetas');
-define("DB_USERNAME", 'root');
-define("DB_PASSWORD", '');
+define("DB_HOST", 'us-cdbr-east-06.cleardb.net');
+define("DB_DATABASE", 'heroku_8196a47a5249e57');
+define("DB_USERNAME", 'b8262c983e5d17');
+define("DB_PASSWORD", 'd4b74594');
 
 
